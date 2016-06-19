26efab90c89984f58c687ad451f047c8b4114c2f
Kevin Orge <kevinorge@gmail.com>
2016-06-19 16:22:14 +0800

    Auth - Changed route path and model

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,24 +2,18 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\User;
+use App\Models\User;
 use Validator;
+use Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 
 class AuthController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Registration & Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users, as well as the
-    | authentication of existing users. By default, this controller uses
-    | a simple trait to add these behaviors. Why don't you explore it?
-    |
-    */
+    protected $loginPath = '/auth/login';
+    protected $redirectPath = '/';
+    private $messages = [];
 
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
@@ -41,6 +35,9 @@ class AuthController extends Controller
      */
     protected function validator(array $data)
     {
+        //if($data['password'] !== $data['confirm_password'])
+        //    return false;
+
         return Validator::make($data, [
             'name' => 'required|max:255',
             'email' => 'required|email|max:255|unique:users',
@@ -56,21 +53,18 @@ class AuthController extends Controller
      */
     protected function create(array $data)
     {
-        /*return User::create([
-            'name' => $data['name'],
+        return User::create([
+            'firstname' => $data['firstname'],
+            'lastname' => $data['lastname'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
-        ]);*/
-        $_COOKIE['username'] = $data['username'];
-        $_COOKIE['password'] = $data['password'];
+        ]);
     }
 
     public function login(array $data){
-        print_r($data);exit();
         if($_COOKIE['username'] == $data['username'] && $_COOKIE['password'] == $data['password']){
-            session()
             return redirect('/');
         }
-        return redirect('/auth')->with('error', 'Wrong username/password.');
+        return redirect('/auth/login')->with('error', 'Wrong username/password.');
     }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,6 +13,21 @@
 
 Route::get('/', 'HomeController@index');
 
+/*
+Route::get('/login', 'UserController@loginView');
+Route::post('/login', 'Auth\AuthController@login');
+Route::get('/register', 'UserController@registerView');
+Route::post('/register', 'UserController@register');
+*/
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
 Route::get('/customers', 'CustomerController@index');
 Route::get('/customers/form', 'CustomerController@addForm');
 Route::get('/customers/form/{ID}', 'CustomerController@editForm');
