f9e83b0800d94777276b34287736496dc144b30c
Kevin Orge <kevinorge@gmail.com>
2016-06-19 16:35:02 +0800

    Customer - Add password and confirm password fields.

--- resources/views/customers/form.blade.php
+++ resources/views/customers/form.blade.php
@@ -8,3 +8,11 @@
 	<input type="text" name="lastname" id="lastname" value="{{$customer->lastname}}"/>
 	<label for="lastname">Last Name</label>
 </div>
+<div class="input-field col s12 m6 l6">
+	<input type="text" name="password" id="password" value="{{$customer->password}}"/>
+	<label for="password">Password</label>
+</div>
+<div class="input-field col s12 m6 l6">
+	<input type="text" name="confirm_password" id="confirm_password" value=""/>
+	<label for="confirm_password">Confirm Password</label>
+</div>
